abe0a2ea499f8a07fe21cf23bcd294f34e265ad4
Kieran McCormick <octavian13@shaw.ca>
2021-10-07 12:19:47 -0700

    added sol for 2015 day 1

--- 2015/day1.cxx
+++ 2015/day1.cxx
@@ -1,8 +1,42 @@
-// Your First C++ Program
-
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::ostringstream;
+
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+             << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
 
 int main() {
-    std::cout << "Hello World!";
+    int floor = 0;
+
+    string filename("2015/day1input.txt");
+    string file_contents;
+
+    file_contents = readFileIntoString(filename);
+
+    for (string::size_type i = 0;  i < file_contents.size(); i++ )
+    {
+      if (file_contents[i] == ')')
+      {
+          floor--;
+      }
+      else
+      {
+          floor++;
+      }   
+    }
+
+    std::cout << floor;
     return 0;
 }
