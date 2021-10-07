b1442441434162402ddbdb151897835676a1eb89
Kieran McCormick <octavian13@shaw.ca>
2021-10-07 12:27:50 -0700

    added day 1.5 solution

---
+++ 2015/day1-5.cxx
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+using std::cout; using std::cerr;
+using std::endl; using std::string;
+using std::ifstream; using std::ostringstream;
+
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+             << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
+int main() {
+    int floor = 0;
+    int position = 0;
+    string filename("2015/day1input.txt");
+    string file_contents;
+
+    file_contents = readFileIntoString(filename);
+
+    for (string::size_type i = 0;  i < file_contents.size(); i++ )
+    {
+        position++;
+        if (file_contents[i] == ')')
+        {
+            floor--;
+        }
+        else
+        {
+            floor++;
+        }
+
+        if (floor == -1)
+        {
+            std::cout << position;
+            return 0;
+        }
+    }
+    return 0;
+}
